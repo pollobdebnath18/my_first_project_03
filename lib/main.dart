1e6abcfd133f6c34bcb1ff7996f00df6f4dba455
pollob debnath <pullobdebnath@gmail.com>
2025-04-21 19:17:15 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_application_03/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter App",
+      theme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
